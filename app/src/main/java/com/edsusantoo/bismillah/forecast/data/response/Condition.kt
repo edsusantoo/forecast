a593a14f065ce23a55155ba593bbeac1bcc5a8b3
edsusantoo <edsusantoo@gmail.com>
2019-01-13 11:05:08 +0700

    membuat response current weather

---
+++ app/src/main/java/com/edsusantoo/bismillah/forecast/data/response/Condition.kt
@@ -0,0 +1,7 @@
+package com.edsusantoo.bismillah.forecast.data.response
+
+data class Condition(
+    val code: Int,
+    val icon: String,
+    val text: String
+)
---
+++ app/src/main/java/com/edsusantoo/bismillah/forecast/data/response/CurrentWeatherResponse.kt
@@ -0,0 +1,9 @@
+package com.edsusantoo.bismillah.forecast.data.response
+
+import com.google.gson.annotations.SerializedName
+
+data class CurrentWeatherResponse(
+    @SerializedName("current")
+    val currentWeatherEntry: CurrentWeatherEntry,
+    val location: Location
+)
