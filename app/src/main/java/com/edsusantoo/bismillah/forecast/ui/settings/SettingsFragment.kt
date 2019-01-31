c744e53d729e715bdc5285ec1aa4a77d29a55b10
edsusantoo <edsusantoo@gmail.com>
2019-01-31 14:18:54 +0700

    membuat taampilan setting fragment menggunakan preferences xml

--- app/src/main/java/com/edsusantoo/bismillah/forecast/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/edsusantoo/bismillah/forecast/ui/settings/SettingsFragment.kt
@@ -1,7 +1,18 @@
 package com.edsusantoo.bismillah.forecast.ui.settings
 
-import androidx.fragment.app.Fragment
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.preference.PreferenceFragmentCompat
+import com.edsusantoo.bismillah.forecast.R
 
-class SettingsFragment : Fragment() {
+class SettingsFragment : PreferenceFragmentCompat() {
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        addPreferencesFromResource(R.xml.preferences)
+    }
 
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        (activity as? AppCompatActivity)?.supportActionBar?.title = "Settings"
+        (activity as? AppCompatActivity)?.supportActionBar?.subtitle = null
+    }
 }
