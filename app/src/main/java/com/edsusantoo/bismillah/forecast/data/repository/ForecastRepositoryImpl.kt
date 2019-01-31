587a6b8da654c4bc8bef06ac88cb1113ecde745b
edsusantoo <edsusantoo@gmail.com>
2019-01-31 13:54:06 +0700

    membuat tampilan current wheater dan menset data ketampilan

--- app/src/main/java/com/edsusantoo/bismillah/forecast/data/repository/ForecastRepositoryImpl.kt
+++ app/src/main/java/com/edsusantoo/bismillah/forecast/data/repository/ForecastRepositoryImpl.kt
@@ -46,7 +46,7 @@ class ForecastRepositoryImpl(
 
     private suspend fun fetchCurrentWeather() {
         weatherNetworkDataSource.fetchCurrentWeather(
-            "Los Angles",
+            "Jakarta",
             Locale.getDefault().language
         )
     }
--- app/src/main/java/com/edsusantoo/bismillah/forecast/ui/weather/current/CurrentWeatherFragment.kt
+++ app/src/main/java/com/edsusantoo/bismillah/forecast/ui/weather/current/CurrentWeatherFragment.kt
@@ -1,11 +1,14 @@
 package com.edsusantoo.bismillah.forecast.ui.weather.current
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import com.bumptech.glide.Glide
 import com.edsusantoo.bismillah.forecast.R
 import com.edsusantoo.bismillah.forecast.ui.base.ScopedFragment
 import kotlinx.android.synthetic.main.current_weather_fragment.*
@@ -45,8 +48,64 @@ class CurrentWeatherFragment : ScopedFragment(), KodeinAware {
         currentWeather.observe(this@CurrentWeatherFragment, Observer {
             if (it == null) return@Observer //jika observer null maka akan merequestnya kembali
 
-            txt_current.text = it.toString()
+            group_loading.visibility = View.GONE
+
+            updateLocation("Jakarta")
+            updateDateToToday()
+            updateTemperatures(it.temperature, it.feelsLikeTemperature)
+            updateCondition(it.conditionText)
+            updatePrecipitation(it.precipitationVolume)
+            updateWind(it.windDirection, it.windSpeed)
+            updateVisibility(it.visibilityDistance)
+
+            Glide.with(this@CurrentWeatherFragment)
+                .load("http:${it.conditionIconUrl}")
+                .into(img_condition_icon)
+
         })
+    }
+
+    private fun chooseLocalizedUnitAbbrevation(metric:String,imperial:String):String{
+        return if(viewModel.isMetric) metric else imperial
+    }
+
+    @Suppress("CAST_NEVER_SUCCEEDS")
+    private fun updateLocation(location: String) {
+        (activity as? AppCompatActivity)?.supportActionBar?.title = location
+    }
+
+    private fun updateDateToToday() {
+        (activity as? AppCompatActivity)?.supportActionBar?.subtitle = "Today"
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun updateTemperatures(temperature: Double, feelsLike: Double) {
+        val unitAbbreviation = chooseLocalizedUnitAbbrevation("°C","°F")
+        txt_temperature.text="$temperature$unitAbbreviation"
+        txt_feels_like_temperature.text="Feels like $feelsLike$unitAbbreviation"
+    }
+
+    private fun updateCondition(condition:String){
+        txt_condition.text=condition
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun updatePrecipitation(precipitationVolume:Double){
+        val unitAbbreviation = chooseLocalizedUnitAbbrevation("mm","in")
+        txt_precipitation.text="Precipitaion: $precipitationVolume $unitAbbreviation"
+
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun updateWind(windDirection: String, windSpeed: Double) {
+        val unitAbbreviation = chooseLocalizedUnitAbbrevation("kph", "mph")
+        txt_wind.text = "Wind: $windDirection, $windSpeed $unitAbbreviation"
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun updateVisibility(visibilityDistance: Double) {
+        val unitAbbreviation = chooseLocalizedUnitAbbrevation("km", "mi.")
+        txt_visibility.text = "Visibility: $visibilityDistance $unitAbbreviation"
     }
 
 }
